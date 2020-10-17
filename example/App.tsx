57b22bbefa2ad05573187f01cc6c660bfd42d5b2
Vipin <6302771+vip-git@users.noreply.github.com>
2020-10-17 16:44:40 +0200

    feat: example folder updated

---
+++ example/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import { TodosContextProvider } from './contexts/TodosContext';
+
+const App = () => {
+  return (
+			<TodosContextProvider>
+				<TodoForm />
+				<TodoList />
+			</TodosContextProvider>
+	);
+};
+
+export default App;
