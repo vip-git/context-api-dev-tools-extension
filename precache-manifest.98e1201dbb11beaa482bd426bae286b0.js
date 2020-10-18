8bb7ee7d0f2dc92dc3746b8f449ea0d78b8ed03d
vip-git <6302771+vip-git@users.noreply.github.com>
2020-10-18 01:38:51 +0000

    Deploying to gh-pages from  @ a0e4bab9d79a520629a623b328747e9128b4546d 🚀

---
+++ precache-manifest.98e1201dbb11beaa482bd426bae286b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa8531e45a10de3ad2e3ba685ff1f73",
+    "url": "/context-api-dev-tools-extension/index.html"
+  },
+  {
+    "revision": "c9b2f3ce4891bbbdd9df",
+    "url": "/context-api-dev-tools-extension/static/css/main.a093413b.chunk.css"
+  },
+  {
+    "revision": "df7e59496b8f0510a2dc",
+    "url": "/context-api-dev-tools-extension/static/js/2.c7cc9574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/context-api-dev-tools-extension/static/js/2.c7cc9574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b2f3ce4891bbbdd9df",
+    "url": "/context-api-dev-tools-extension/static/js/main.8c4dd2d4.chunk.js"
+  },
+  {
+    "revision": "2dc559ebbb67190e1c79",
+    "url": "/context-api-dev-tools-extension/static/js/runtime-main.52c9d508.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/context-api-dev-tools-extension/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4faed9a27fa49458289e4c6bcd778f40.js"
+  "/context-api-dev-tools-extension/precache-manifest.98e1201dbb11beaa482bd426bae286b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/context-api-dev-tools-extension/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
